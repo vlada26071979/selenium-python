b67b398687c420b75059ced9039f41bbf4d3a4a8
= <vladimir.djordjevic@in4nova.com>
2023-10-02 13:14:43 +0200

    Groovy file added

---
+++ Jenkins.groovy
@@ -0,0 +1,69 @@
+
+
+pipeline {
+
+    agent any
+
+    stages {
+
+        stage('Checkout and Pull') {
+
+            steps {
+
+ script {
+                    
+                    dir('C:\\Users\\VladimirDjordjevic\\Desktop\\razno\\Faljlovi sa Git hub-a\\selenium-python') {
+                         
+bat 'git config --global --add safe.directory C:\\Users\\VladimirDjordjevic\\Desktop\\razno\\Faljlovi sa Git hub-a\\selenium-python'
+                        bat 'git clean -f -d -x'
+
+ 
+
+                        bat 'git pull'
+                    }
+                }
+
+            }
+
+        }
+
+ 
+
+        stage('Run Selenium Scenario') {
+
+            steps { 
+script {
+                    
+                    dir('C:\\Users\\VladimirDjordjevic\\Desktop\\razno\\Faljlovi sa Git hub-a\\selenium-python') {
+                         
+
+
+ 
+
+                        bat 'python test_login.py'
+                    }
+                }
+
+                
+
+                   
+
+                    
+
+                   
+
+                
+
+            }
+
+        }
+
+ 
+
+ 
+
+ 
+
+    }
+
+}
